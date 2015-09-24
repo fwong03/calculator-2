f0fefb7d15f1b16e9483637813d3203ed949c4bb
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 18:02:35 -0700

    breaks when too many args

--- calculator.py
+++ calculator.py
@@ -12,17 +12,38 @@ while True:
     line = raw_input("> ")
     tokens = line.split(" ")
     
-    operator = tokens[0]
-    num1 = int(tokens[1])
-    num2 = int(tokens[2])
-
-    if operator == "q":
+    if tokens[0] == "q":
+        print "ending program"
         break
-    elif operator == "+":
-        print add(num1, num2)
-    elif operator == "-":
-        print subtract(num1, num2)
+    
 
+    else:
+        operator = tokens[0]
+        num1 = int(tokens[1])
+
+        if len(tokens) > 2:
+           num2 = int(tokens[2])
+
+        if operator == "q":
+            break
+        elif operator == "+":
+            print add(num1, num2)
+        elif operator == "-":
+            print subtract(num1, num2)
+        elif operator == "*":
+            print multiply(num1, num2)
+        elif operator == "/":
+            print divide(num1, num2)
+        elif operator == "square":
+            print square(num1)
+        elif operator == "cube":
+            print cube(num1)
+        elif operator == "pow":
+            print power(num1, num2)
+        elif operator == "mod":
+            print mod(num1, num2)
+        else:
+            print "I don't understand."
     
 
 
